316a363717d62aee425df7996a12d046abfc1afc
amirul-yol <ahj.professional@gmail.com>
2025-04-17 12:27:55 +0800

    Add role-based access control for LO in add cards

--- views/card/index.php
+++ views/card/index.php
@@ -1,9 +1,68 @@
-<?php include 'views/includes/header.php'; ?>
+<?php 
+require_once 'controllers/AuthController.php';
+// Create Auth Controller instance to check user roles
+$authController = new AuthController();
+$isLO = $authController->isLogisticsOfficer();
+include 'views/includes/header.php'; 
+?>
 <?php include 'views/includes/sidebar.php'; ?>
+
+<style>
+    .add-card-btn.disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+        pointer-events: none;
+    }
+    
+    .tooltip {
+        position: relative;
+        display: inline-block;
+    }
+    
+    .tooltip .tooltip-text {
+        visibility: hidden;
+        width: 220px;
+        background-color: #555;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px;
+        position: absolute;
+        z-index: 1;
+        bottom: 125%;
+        left: 50%;
+        margin-left: -110px;
+        opacity: 0;
+        transition: opacity 0.3s;
+    }
+    
+    .tooltip .tooltip-text::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
+    }
+    
+    .tooltip:hover .tooltip-text {
+        visibility: visible;
+        opacity: 1;
+    }
+</style>
 
 <div class="content">
     <h1>Card Management 
-        <a href="index.php?path=card/create" class="btn btn-primary add-card-btn">Add Card</a>
+        <?php if ($isLO): ?>
+            <a href="index.php?path=card/create" class="btn btn-primary add-card-btn">Add Card</a>
+        <?php else: ?>
+            <div class="tooltip">
+                <a class="btn btn-primary add-card-btn disabled">Add Card</a>
+                <span class="tooltip-text">Only Logistics Officers can add cards</span>
+            </div>
+        <?php endif; ?>
     </h1>
     <div class="bank-list">
         <?php foreach ($banks as $bankId => $bank): ?>
