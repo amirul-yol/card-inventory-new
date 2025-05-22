f71c322ea8d04700ff71f131c5ed0c22a73bfe2c
amirul-yol <ahj.professional@gmail.com>
2025-05-22 09:11:53 +0800

    update: add role-based logic to hide Add Card button in Card Page

--- views/card/index.php
+++ views/card/index.php
@@ -122,7 +122,9 @@ include 'views/includes/header.php';
             </div>
         <?php else: ?>
             <div class="filter-options">
-                <a href="index.php?path=card/create" class="btn btn-primary add-card-btn">Add Card</a>                
+                <?php if (!$authController->isBank()): ?>
+                    <a href="index.php?path=card/create" class="btn btn-primary add-card-btn">Add Card</a>                
+                <?php endif; ?>
                 <span>Filter by type: </span>
                 <a href="index.php?path=card&type=CREDIT CARD" class="btn btn-outline">Credit Cards</a>
                 <a href="index.php?path=card&type=DEBIT CARD" class="btn btn-outline">Debit Cards</a>
