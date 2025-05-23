f4b729bdf2ae8536d3fe90a48f2f3ef235ce850a
amirul-yol <ahj.professional@gmail.com>
2025-05-23 13:00:50 +0800

    add banks logo & increase bankCards image size

--- views/components/BankCard.php
+++ views/components/BankCard.php
@@ -3,7 +3,7 @@
 // Expects: $bankLogoUrl, $bankName, $cardsValue
 ?>
 <div class="card h-100 shadow-sm">
-  <img src="<?= htmlspecialchars($bankLogoUrl) ?>" class="card-img-top p-3" alt="<?= htmlspecialchars($bankName) ?> Logo" style="max-height: 80px; object-fit: contain; margin-left: auto; margin-right: auto;">
+  <img src="<?= htmlspecialchars($bankLogoUrl) ?>" class="card-img-top p-3 bank-card-logo-img" alt="<?= htmlspecialchars($bankName) ?> Logo">
   <div class="card-body text-center d-flex flex-column justify-content-center">
     <h5 class="card-title"><?= htmlspecialchars($bankName) ?></h5>
     <p class="card-text">
